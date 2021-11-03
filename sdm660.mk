94c5e4a21e8273f564226f2263b03091ef73a169
pkm774 <mprabhat774@gmail.com>
2021-11-03 16:06:31 +0530

    sdm660-common: Build OSS Camera HAL
    Change-Id: Ia5b934aa1c42608a16d888984eac464c27090851
    Signed-off-by: pkm774 <mprabhat774@gmail.com>
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- sdm660.mk
+++ sdm660.mk
@@ -120,6 +120,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     libdng_sdk.vendor \
     vendor.qti.hardware.camera.device@1.0:64
+    camera.sdm660
 
 # Cgroup and task_profiles
 PRODUCT_COPY_FILES += \
