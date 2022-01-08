a56ae54c1696db216f17ae51928b24ac1e518724
NurKeinNeid <mralexman3000@gmail.com>
2022-01-08 19:23:57 +0100

    sdm660-common: Derp it
    Change-Id: Ib1ab5cd9021f19fc9554992fdb0c427c55f291b4
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -6,7 +6,10 @@
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-derp
+
+PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
