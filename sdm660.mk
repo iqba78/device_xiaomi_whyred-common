d4f71b8e556bae58cc3d9f9f1769a2902544f17f
iqbal <mi460334@gmail.com>
2023-06-21 06:20:47 +0000

    build camera

--- sdm660.mk
+++ sdm660.mk
@@ -121,7 +121,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
     libdng_sdk.vendor \
-    vendor.qti.hardware.camera.device@1.0:64
+    vendor.qti.hardware.camera.device@1.0
 
 # Cgroup and task_profiles
 PRODUCT_COPY_FILES += \
